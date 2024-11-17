41b0f08860d309a8a448b18f2d8e75e802b5d5f6
Greg.Study <greg0917@naver.com>
2024-11-18 00:50:20 +0900

    lec06

---
+++ java-to-kotlin-starter-guide-master/src/main/kotlin/com/lannstark/lec06/Lec05Main.kt.kt
@@ -0,0 +1,28 @@
+package com.lannstark.lec06
+
+// 반복문
+fun main() {
+    val numbers = listOf(1, 2, 3)
+    for (number in numbers) {
+        println(number)
+    }
+
+    for (i in 1..3) {
+        println(i)
+    }
+
+    for (i in 3 downTo 1) {
+        println(i)
+    }
+
+    for (i in 1..5 step 2) {
+        println(i)
+    }
+    
+    var i = 1
+    while (i <= 3) {
+        println(i)
+        i++
+    }
+
+}
