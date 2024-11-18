ebff2dc0b6fbe2723268ec30ed811ebafa48229d
Greg.Study <greg0917@naver.com>
2024-11-19 01:49:09 +0900

    lec09

---
+++ java-to-kotlin-starter-guide-master/src/main/kotlin/com/lannstark/lec09/Lec09Main.kt
@@ -0,0 +1,26 @@
+package com.lannstark.lec09
+
+fun main() {
+    val person = Person("greg", 100)
+    println(person.name)
+    person.age = 10
+    println(person.age)
+
+}
+
+class Person (
+    val name: String = "greg",
+    var age: Int = 1
+) {
+    init {
+        if (age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다.")
+        }
+    }
+
+    fun getUppercaseName(): String = this.name.uppercase()
+
+    val isAdult: Boolean 
+        get() = this.age >= 20
+
+}
