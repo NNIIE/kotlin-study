9e4e6a1ed93fca34631e49cba8f82e95acbf8932
Greg.Study <greg0917@naver.com>
2024-11-22 01:35:20 +0900

    lec17

---
+++ java-to-kotlin-starter-guide-master/src/main/kotlin/com/lannstark/lec17/Lec17Main.kt
@@ -0,0 +1,39 @@
+package com.lannstark.lec17
+
+// 람다
+fun main() {
+    var fruits = listOf(
+        Fruit("사과", 1000),
+        Fruit("사과", 1200),
+        Fruit("사과", 1200),
+        Fruit("사과", 1500),
+        Fruit("바나나", 3000),
+        Fruit("바나나", 3200),
+        Fruit("바나나", 2500),
+        Fruit("수박", 10000)
+    )
+
+    val isApple: (Fruit) -> Boolean = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+
+    val isApple2: (Fruit) -> Boolean = {
+        fruit: Fruit -> fruit.name == "사과"
+    }
+
+    filterFruits(fruits, isApple)
+    filterFruits(fruits) { it.name == "사과" }
+
+}
+
+private fun filterFruits(
+    fruits: List<Fruit>, filter: (Fruit) -> Boolean
+): List<Fruit> {
+    var results = mutableListOf<Fruit>()
+    for (fruit in fruits) {
+        if (filter(fruit)) {
+            results.add(fruit)
+        }
+    }
+    return results
+}
